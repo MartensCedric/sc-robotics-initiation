186feb28e08874109c76b34509c14f58bb5d47b1
MartensCedric <cedricmartens98@gmail.com>
2019-07-06 16:31:41 -0400

    Added potentiometer and button

--- binary_clock/binary_clock.ino
+++ binary_clock/binary_clock.ino
@@ -1,22 +1,35 @@
 const int MAX_NUMBER = 255;
 const int LED_AMOUNT = 8;
 const int LED_DISPLACEMENT = 2;
-const unsigned long DELAY = 1000;
+const int POTENTIOMETER_PIN = A0;
+const int RESET_PIN = 10;
+unsigned long delayAmount = 1000;
 int current = 0;
 unsigned long lastTime = 0;
 
 
 void setup() {
-  Serial.begin(9600);
   for(int i = 0; i < LED_AMOUNT; i++)
     pinMode(i + LED_DISPLACEMENT, OUTPUT);
+
+  pinMode(RESET_PIN, INPUT);
+  pinMode(13, OUTPUT);
 
   lastTime = millis();
 }
 
 void loop() {
   unsigned long currentTime = millis();
-  if(currentTime >= lastTime + DELAY)
+  delayAmount = analogRead(POTENTIOMETER_PIN) + 100;
+  if(digitalRead(RESET_PIN) == HIGH)
+  {
+    reset();
+    digitalWrite(13, HIGH);
+  }
+  else
+    digitalWrite(13, LOW);
+    
+  if(currentTime >= lastTime + delayAmount)
   {
     display(current);
     setNextNumber();
@@ -41,4 +54,9 @@ int setNextNumber()
     current += 1;
   else
     current = 0;
+}
+void reset()
+{
+  current = 0;
+  lastTime = millis();
 }
