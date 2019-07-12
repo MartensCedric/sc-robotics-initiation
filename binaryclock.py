0e3a6dd69bf9b33f0a444941695d53ce1523368e
MartensCedric <cedricmartens98@gmail.com>
2019-07-12 00:28:56 -0400

    Fixed bug where everything would work in Py3 but not 2 as used

--- binaryclock.py
+++ binaryclock.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, redirect, request, url_for
 import serial
 import sys
+import struct
 
 app = Flask(__name__)
 
 RESET = 1
+PORT = "/dev/ttyUSB1"
 
-port = "/dev/ttyUSB1"
-print("Connecting to :" + port)
-ser = serial.Serial(port,baudrate = 9600, timeout=1)
+print("Connecting to :" + PORT)
+ser = serial.Serial(PORT, baudrate = 9600, timeout=1)
 
 @app.route("/")
 def index():
@@ -16,5 +17,5 @@ def index():
 
 @app.route("/reset")
 def reset():
-	ser.write(bytes([RESET]))
+	ser.write(struct.pack('>B', RESET))
 	return redirect(url_for('index'))
